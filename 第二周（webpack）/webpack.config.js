70d6e234d905c18dddf64e1653ff507f8c4c7171
qiuliang <1043270224@qq.com>
2020-05-26 15:41:20 +0800

    提交作业

---
+++ 第二周（webpack）/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    // path.join() 去拼接路径
+    // __dirname 当前文件的绝对路径
+    filename: 'bundle.js',
+    path: path.join(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        // sass-loader node-sass两个依赖都需要安装
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = config
